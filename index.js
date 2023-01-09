b89d4e2ae9465d88e14488ab1b648b29b8cd878c
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-01-09 22:07:59 +0800

    some change

--- index.js
+++ index.js
@@ -14,7 +14,16 @@ client.on('ready', () => {
     console.log('Client is ready!');
 });
 
+client.on('message', async message => {
+  const content = message.body;
+  const from = message.from.split("@").shift();
+  const date = new Date();
+  date.setTime(date.getTime() + date.getTimezoneOffset() * 60 * 1000 + 16 * 60 * 60 * 1000);
+  const dateNow = date.toISOString().substring(0, 19).replace('T', ' ');
+  const con = await database.getConnection();
 
+  
+});
 
 client.initialize();
  
