786c31791decb8667c8a5c7f7b2ffb38dea514e1
Monochrome Taisa <44545985+idm1303@users.noreply.github.com>
2023-01-09 22:04:09 +0800

    some change

--- index.js
+++ index.js
@@ -14,68 +14,7 @@ client.on('ready', () => {
     console.log('Client is ready!');
 });
 
-client.on('message', async message => {
-  const content = message.body;
-  const from = message.from.split("@").shift();
-  const date = new Date();
-  date.setTime(date.getTime() + date.getTimezoneOffset() * 60 * 1000 + 16 * 60 * 60 * 1000);
-  const dateNow = date.toISOString().substring(0, 19).replace('T', ' ');
-  const con = await database.getConnection();
 
-  switch (true) {
-    case content === '!info':
-      database.getCandidateInfo(con)
-        .then(results => {
-          let msgs = ``;
-          results.forEach(element => {
-            msgs += "\n\nnomor urut : "+element.id_kandidat+ "\nnama : "+element.nama_kandidat+"\njml suara saat ini : "+element.poin;
-          });
-          let ket = "\n\nKetik '!vote-_nomor urut kandidat_' untuk memilih kandidat \ncontoh : *!vote-1*";
-          msgs = "INFO KANDIDAT!" + msgs + ket;
-          client.sendMessage(message.from, msgs);
-        })
-        .catch(console.error);
-      break;
-    case content.slice(0, 6) === '!vote-':
-    
-      let candidateId = content.split('-').pop();
-      const candidates = await database.getCandidates(con);
-      const voters = await database.getContactsVoters(con);
-      const already_vote = await database.getAlreadyVote(con);
-      const voter = voters.find(voter => voter.no_pemilih === from);
-      const unregis = voter !== undefined;
-      console.log(unregis);
-      if (!unregis) {
-        client.sendMessage(message.from, `you're registered in this voting system. please register first!`);
-        return;
-      }
-
-      const hasVoted = candidates.some(candidate => already_vote.some(voter => voter.no_pemilih === candidate.no_pemilih));
-      // console.log(hasVoted);
-      if (hasVoted) {
-        client.sendMessage(message.from, `you've already voted. you can only vote once!`);
-        return;
-      }
-
-      try {
-        if (candidateId > candidates.length) {
-          client.sendMessage(message.from, "there's no such candidate");
-          return;
-        }
-        const candidate = candidates.find(el => el.id_kandidat == candidateId);
-        if (!candidate) return;
-        
-        await database.insertDataVote(con, from, candidate.id_voting, candidate.id_kandidat, dateNow);
-        client.sendMessage(message.from, `voting succes ${candidateId}`);
-      } catch (error) {
-        console.log(error)
-      }
-        break;
-    default:
-      // TODO :: change this default msg to the help info
-      client.sendMessage(message.from, `I'm sorry for this annoying reply. pls wait until i reply ur msg by myself!`);
-  }
-});
 
 client.initialize();
  
